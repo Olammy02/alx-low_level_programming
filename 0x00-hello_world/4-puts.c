61e642d3aeba9e0d9cc6600348a605014cada176
Olammy02 <olammy02@gmail.com>
2022-04-08 08:50:48 -0700

    Task 4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,4 +1,4 @@
-#include <stdio.h>
+"#include <stdio.h>
 /**
  *main - entry point
  *
@@ -6,6 +6,6 @@
  */
 int main(void)
 {
-puts("\"Programming is like build house");
+puts("\"Programming is like building a multilingual puzzle");
 return (0);
 }
