d27b647e868a234f7e0adbdf74026991f04e1384
Olammy02 <olammy02@gmail.com>
2022-04-12 15:30:31 -0700

    Task 1

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * main - check the code
+ *
+ * Printing alphabet in lowercase
+ */
+void print_alphabet(void)
+{
+	char d;
+	for (d = 'a'; d <= 'z'; d++)
+	_putchar(d);
+	_putchar('\n');
+}
