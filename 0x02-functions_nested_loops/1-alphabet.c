ee537b086b531ef1e7ea0c27c50cb9623417633a
Olammy02 <olammy02@gmail.com>
2022-04-13 12:42:06 -0700

    Task Real 1

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,15 +1,21 @@
 #include "main.h"
 
 /**
-* main - check the code
-*
-* Printing alphabet in lowercase
-*/
+ * print_alphabet - print alphabet lowercase
+ *
+ * Return: Always 0.
+ */
+
 void print_alphabet(void)
 {
-char d;
-for (d = 'a'; d <= 'z'; d++)
-_putchar(d);
-_putchar('\n');
+char alphabet;
+
+for (alphabet = 'a'; alphabet <= 'z'; alphabet++)
+{
+_putchar(alphabet);
 }
 
+_putchar('\n');
+
+}
+© 2022 GitHub, Inc.
