591042bfc9a89b9eb3fa4aaa2c31dac5fa5f1de6
Olammy02 <olammy02@gmail.com>
2022-04-13 12:11:37 -0700

    Task 01 New

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,11 +1,11 @@
 #include "main.h"
 
 /**
- * print_alphabet_x10 - print alphabet
+ * alphabet_x10 - print alphabet
  *
  * Return: Always 0.
  */
-void print_alphabet_x10(void)
+void alphabet_x10(void)
 {
 int alphabet;
 int count;
