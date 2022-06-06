7012cbe32f4e2cd0e1bef09da0c7fa76944eb2f7
Olammy02 <olammy02@gmail.com>
2022-06-06 02:53:14 -0700

    Task 09

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,24 @@
+table File  22 lines (20 sloc)  286 Bytes
+   
+#include <stdio.h>
+
+/**
+ * main - prints all possible combinations of single-digit numbers
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int n;
+
+	for (n = 48; n < 58; n++)
+	{
+		putchar(n);
+		if (n != 57)
+		{
+			putchar(',');
+			putchar(' ');
+		}
+	}
+	putchar('\n');
+	return (0);
+}
